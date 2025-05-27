338ff7149b09b3c2c4c973550bcf86507bde8b96
Dzmitry Shatokhin <dshatokh@gmail.com>
2025-05-27 17:58:31 +0200

    Fixed unit issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
+<!-- src/routes/+page.svelte -->
 <script lang="ts">
   interface ClassMenuItem {
     id: string;
@@ -12,9 +13,9 @@
   ];
 
   const row2: ClassMenuItem[] = [
-    { id: 'shadow',  nameRU: 'Тень',    nameEN: 'Shadow', iconPath: '/images/classes/shadow.png' },
-    { id: 'hunter',  nameRU: 'Охотник', nameEN: 'Hunter', iconPath: '/images/classes/hunter.png' },
-    { id: 'mage',    nameRU: 'Маг',     nameEN: 'Mage',   iconPath: '/images/classes/mage.png' }
+    { id: 'shadow', nameRU: 'Тень',    nameEN: 'Shadow', iconPath: '/images/classes/shadow.png' },
+    { id: 'hunter', nameRU: 'Охотник', nameEN: 'Hunter', iconPath: '/images/classes/hunter.png' },
+    { id: 'mage',   nameRU: 'Маг',     nameEN: 'Mage',   iconPath: '/images/classes/mage.png' }
   ];
 </script>
 
@@ -26,9 +27,9 @@
     class="absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2
            flex flex-col items-center gap-y-[75px]"
   >
-    <!-- Первый ряд -->
+    <!-- Первый ряд: 2 иконки -->
     <div class="flex gap-x-[75px]">
-      {#each row1 as item}
+      {#each row1 as item (item.id)}
         <a
           href={`/class/${item.id}`}
           class="flex flex-col items-center"
@@ -59,9 +60,9 @@
       {/each}
     </div>
 
-    <!-- Второй ряд -->
+    <!-- Второй ряд: 3 иконки -->
     <div class="flex gap-x-[75px]">
-      {#each row2 as item}
+      {#each row2 as item (item.id)}
         <a
           href={`/class/${item.id}`}
           class="flex flex-col items-center"
